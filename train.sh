6a4671400bea2a536a1fea8c7b3c5e6b53421fb0
Jongseong Woo <22februa@gmail.com>
2019-01-22 18:22:49 +0900

    modified arguments

--- train.sh
+++ train.sh
@@ -1,9 +1,14 @@
-#! /bin/bash
+#! /bin/sh
 
-data=PATH_TO_DATA #`pwd`
-python code/trainer.py --input $data/train.32k.en.shuf $data/train.32k.de.shuf \
-        --model transformer --output train \
-        --vocabulary $data/vocab.32k.en.txt $data/vocab.32k.de.txt \
-        --validation $data/dev.32k.en \
-        --references $data/dev.32k.de \
-        --parameters=batch_size=3125,device_list=[0],eval_steps=5000,train_steps=100000,save_checkpoint_steps=1500,shared_embedding_and_softmax_weights=true,shared_source_target_embedding=false,update_cycle=8,aan_mask=True,use_ffn=False
+data_dir=/data/pos_sejong800k/s2s  # `pwd`
+train_dir=/data/aan_train/pos_sejong800k_aan_slash_v1
+
+export CUDA_VISIBLE_DEVICES=0
+
+python code/trainer.py \
+  --input $data_dir/pos_sejong800k.train.inputs.encoded $data_dir/pos_sejong800k.train.targets.encoded \
+  --model transformer --output $train_dir \
+  --vocabulary $data_dir/pos_sejong800k.vocab.inputs.cut $data_dir/pos_sejong800k.vocab.targets.cut \
+  --validation $data_dir/pos_sejong800k.dev.inputs.encoded \
+  --references $data_dir/pos_sejong800k.dev.targets.encoded \
+  --parameters=batch_size=3125,device_list=[0],eval_steps=5000,train_steps=100000,save_checkpoint_steps=1500,shared_embedding_and_softmax_weights=true,shared_source_target_embedding=false,update_cycle=8,aan_mask=True,use_ffn=False
